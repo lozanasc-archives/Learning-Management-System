e724788924495ee3097a2e5a0f0fd841240ed4b7
Sean Christian Lozana <45839462+lozanasc@users.noreply.github.com>
2021-08-12 21:04:18 +0700

    Update Login.Form.js
    Removed unimportant comments

--- frontend/Forms/Login.Form.js
+++ frontend/Forms/Login.Form.js
@@ -1,10 +1,4 @@
 
-/*
- ? Para asa ni?
- * We're gonna render the forms ya'll see at the side from the design dynamically using javascript
- ? Nganong dle mugamit ug .html pages? or nganong dle usahon ra?
- * Scalability, para mas sayon mu add ug delete ug feature or changes sa code
-*/
 import Input from '../components/Input.js';
 import Button from '../components/Button.js';
 import Dashboard from '../Dashboard/Dashboard.js';
